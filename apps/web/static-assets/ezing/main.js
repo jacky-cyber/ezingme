efe8b0b567031b3ef03e4857a0d7b3ad0a326fd5
ljshj <jinshanl@gmail.com>
2016-03-06 10:48:16 +0800

    navbar

---
+++ apps/web/static-assets/ezing/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('.right.menu.open').on("click",function(e){
+        e.preventDefault();
+		//$('.ui.vertical.menu').toggle();
+		//$('.ui.labeled.icon.sidebar')
+  		//.sidebar('toggle');
+	});
+    
+	$('.ui.dropdown').dropdown();
+
+	
+});
